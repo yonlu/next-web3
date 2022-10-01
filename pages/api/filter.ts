61f4e304e703033d0be58bf018167a4928da6f19
yonlu <lss.emailbox@gmail.com>
2022-10-01 11:18:58 -0400

    fix: change next.config to include headers object

--- pages/api/filter.ts
+++ pages/api/filter.ts
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
-import NextCors from 'nextjs-cors';
 
 const prisma = new PrismaClient();
 
@@ -71,15 +70,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Run the cors middleware
-  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  });
-
   try {
     if (req.method === 'GET') {
       let result;
