798af0ebb995668ef7a589ddb34590b2a9b10458
yonlu <lss.emailbox@gmail.com>
2022-10-01 23:44:44 -0400

    cors update

--- next.config.js
+++ next.config.js
@@ -26,6 +26,10 @@ const nextConfig = {
             value:
               'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
         ],
       },
     ];
