f7914653ffc5f6a79b3b0e36bcd2f86fca49a292
yonlu <lss.emailbox@gmail.com>
2022-10-01 20:13:10 -0400

    fix: cors middleware in filter.ts

--- pages/api/filter.ts
+++ pages/api/filter.ts
@@ -1,5 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
+import Cors from 'cors';
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+  origin: true,
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 const prisma = new PrismaClient();
 
@@ -70,6 +96,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   try {
     if (req.method === 'GET') {
       let result;
